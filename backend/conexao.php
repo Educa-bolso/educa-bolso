4bd111802f1df2e0b11bb627b45dac6c65ff5e4e
Caio <caio.rodrigues.barbosa@usp.br>
2021-01-16 14:12:46 -0300

    teste

--- backend/conexao.php
+++ backend/conexao.php
@@ -1,5 +1,4 @@
 <?php 
-<<<<<<< HEAD
   $localhost = "localhost";
   $usuario = "root";
   $senha = "";
@@ -9,11 +8,5 @@
 
   if (!$conexao) {
     die ("Falha no banco de dados: " . mysqli_connect_error());
-=======
-  $connection = mysqli_connect('localhost', 'root', '', 'educabolso');
-
-  if (!$connection) {
-    die ("A conexão não foi realizada" . mysqli_connect_error());
->>>>>>> 8cf99a4af6a7ddae6d2eaebc785da378d70efcc3
   }
 ?>
